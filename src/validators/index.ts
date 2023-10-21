b7c94d937d4201941bbe14d4d8e5a40de5a30f6f
Nikita Kozyrev <n.i.kozyrev@gmail.com>
2023-10-21 13:37:09 +0300

    feat: add notNaN validator

--- src/validators/index.ts
+++ src/validators/index.ts
@@ -3,3 +3,5 @@ export const required = (v: unknown) => !!v;
 export const minLength = (length: number) => (v: string) => v.length >= length;
 
 export const maxLength = (length: number) => (v: string) => v.length <= length;
+
+export const notNaN = (v: unknown) => !isNaN(Number(v));
