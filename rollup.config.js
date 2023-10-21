0cc44a21e15504161c2509d4202288c86578ff0a
Nikita Kozyrev <n.i.kozyrev@gmail.com>
2023-10-21 20:44:13 +0300

    config: add validators chunk

--- rollup.config.js
+++ rollup.config.js
@@ -4,19 +4,12 @@ import typescript from '@rollup/plugin-typescript';
 import dts from 'rollup-plugin-dts';
 import PeerDepsExternalPlugin from 'rollup-plugin-peer-deps-external';
 
-// const packageJson = require('./package.json');
-
 export default [
   {
-    input: 'src/index.ts',
+    input: ['src/index.ts', 'src/validators/validators.ts'],
     output: [
-      // {
-      //   file: packageJson.main,
-      //   format: 'cjs',
-      //   sourcemap: true,
-      // },
       {
-        file: 'dist/esm/index.js', //packageJson.module,
+        dir: 'dist',
         format: 'esm',
         sourcemap: true,
       },
@@ -29,28 +22,7 @@ export default [
     ],
   },
   {
-    input: 'src/validators/index.ts',
-    output: [
-      // {
-      //   file: packageJson.main,
-      //   format: 'cjs',
-      //   sourcemap: true,
-      // },
-      {
-        file: 'dist/esm/validators/index.js', //packageJson.module,
-        format: 'esm',
-        sourcemap: true,
-      },
-    ],
-    plugins: [
-      PeerDepsExternalPlugin(),
-      resolve(),
-      commonjs(),
-      typescript({ tsconfig: './tsconfig.json' }),
-    ],
-  },
-  {
-    input: 'dist/esm/types/index.d.ts',
+    input: 'dist/types/index.d.ts',
     output: [{ file: 'dist/index.d.ts', format: 'esm' }],
     plugins: [dts()],
   },
