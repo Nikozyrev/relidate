6217fead40b404dd5e679eabeb34b1ca1553c61a
Nikita Kozyrev <n.i.kozyrev@gmail.com>
2023-10-21 19:42:47 +0300

    config: add validators to separate file

--- rollup.config.js
+++ rollup.config.js
@@ -29,6 +29,27 @@ export default [
     ],
   },
   {
+    input: 'src/validators/index.ts',
+    output: [
+      // {
+      //   file: packageJson.main,
+      //   format: 'cjs',
+      //   sourcemap: true,
+      // },
+      {
+        file: 'dist/esm/validators/index.js', //packageJson.module,
+        format: 'esm',
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      PeerDepsExternalPlugin(),
+      resolve(),
+      commonjs(),
+      typescript({ tsconfig: './tsconfig.json' }),
+    ],
+  },
+  {
     input: 'dist/esm/types/index.d.ts',
     output: [{ file: 'dist/index.d.ts', format: 'esm' }],
     plugins: [dts()],
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,2 @@
 export { useValidatedForm } from './hooks/use-validated-form';
 export { type Validator } from './types/validator';
-export * from './validators';
